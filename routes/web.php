aa8d11161a224ca65e12c9c6d1b4989a25994cac
bimaaryasatya <145569118+bimaaryasatya@users.noreply.github.com>
2025-03-18 08:26:17 +0700

    [Pertemuan 1] Installasi dan Pengenalan Laravel

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/dashboard', function(){
+    return "Toko";
+});
+
+Route::get('/keranjang', function(){
+    return "Cart";
+});
+
+Route::get('/checkout', function(){
+    return "Checkout";
+});
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
