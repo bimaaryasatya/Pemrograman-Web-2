eef0162a3066d70646c151a70c3163ec9febdeea
bimaaryasatya <145569118+bimaaryasatya@users.noreply.github.com>
2025-04-23 14:47:16 +0700

    Pertemuan 6

---
+++ resources/views/dashboard/categories/index.blade.php
@@ -0,0 +1,54 @@
+<x-layouts.app :title="__('Categories')">
+    <div class="relative mb-6 w-full">
+        <flux:heading size="xl">Product Categories</flux:heading>
+        <flux:subheading size="lg" class="mb-6">Manage data Product Categories</flux:heading>
+        <flux:separator variant="subtle" />
+
+    </div>
+    
+    <div class="flex justify-between items-center mb-4">
+        <div>
+            <form action="{{ route('categories.index') }}" method="get">
+                @csrf
+                <flux:input icon="magnifying-glass" name="q" value="" placeholder="Search Product Categories" />
+            </form>
+        </div>
+        <div>
+            <flux:button icon="plus">
+                <flux:link href="{{ route('categories.create') }}" variant="subtle">Add New Category</flux:link>
+            </flux:button>
+        </div>
+    </div>
+
+    @if(session()->has('successMessage'))
+        <flux:badge color="lime" class="mb-3 w-full">{{session()->get('successMessage')}}</flux:badge>
+    @endif
+
+    <table class="min-w-full leading-normal">
+            <thead>
+                <tr>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        ID
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Image
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Name
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Slug
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Description
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Created At
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+    </table>
+</x-layouts.app>
